7e53a1449248c8828eaead00ce4eadceead2bcf6
Sheeshpaul Kamboj <shekam@microsoft.com>
2019-10-03 00:02:29 -0700

    Updating rollup config
    Updating rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -6,67 +6,45 @@ import typescript from "rollup-plugin-typescript";
 import uglify from "rollup-plugin-uglify-es";
 
 const production = "production";
+
 const development = "development";
+
+const plugins = [
+	resolve(),    // Locate modules using the Node resolution algorithm, for using third party modules in node_modules
+	commonjs(),   // Convert CommonJS modules to ES6, so they can be included in a Rollup bundle
+	typescript(), // Convert TypeScript to JavaScript
+	replace({
+		exclude: "node_modules/**", ENV: JSON.stringify(process.env.NODE_ENV || development),
+	}),
+	(process.env.NODE_ENV === production && uglify())
+];
+
+const output = {
+	name: "ew",
+	format: "iife",
+	esModule: false
+};
 
 export default [
 	// All components bundle
 	{
 		input: "src/main.ts",
-		output: {
-			name: "ew",
-			file: process.env.NODE_ENV === production ? pkg.browser : "dist/everyday-webcomponents.js",
-			format: "iife",
-			esModule: false
-		},
-		plugins: [
-			resolve(),    // Locate modules using the Node resolution algorithm, for using third party modules in node_modules
-			commonjs(),   // Convert CommonJS modules to ES6, so they can be included in a Rollup bundle
-			typescript(), // Convert TypeScript to JavaScript
-			replace({
-				exclude: "node_modules/**", ENV: JSON.stringify(process.env.NODE_ENV || development),
-			}),
-			(process.env.NODE_ENV === production && uglify())
-		]
+		output: { ...output, file: process.env.NODE_ENV === production ? pkg.browser : "dist/everyday-webcomponents.js"	},
+		plugins: plugins
 	},
 
 	// ew-card component bundle
 	{
 		input: "src/components/ew-card/index.ts",
-		output: {
-			name: "ew",
-			file: process.env.NODE_ENV === production ? "dist/everyday-ew-card-webcomponent.min.js" : "dist/everyday-ew-card-webcomponent.js",
-			format: "iife",
-			esModule: false
-		},
-		plugins: [
-			resolve(),
-			commonjs(),
-			typescript(),
-			replace({
-				exclude: "node_modules/**", ENV: JSON.stringify(process.env.NODE_ENV || development),
-			}),
-			(process.env.NODE_ENV === production && uglify())
-		]
+		output: { ...output, file: process.env.NODE_ENV === production ? "dist/everyday-ew-card-webcomponent.min.js" : "dist/everyday-ew-card-webcomponent.js" },
+		plugins: plugins
 	},
 
 	// ew-image-card component bundle
 	{
 		input: "src/components/ew-image-card/index.ts",
-		output: {
-			name: "ew",
-			file: process.env.NODE_ENV === production ? "dist/everyday-ew-image-card-webcomponent.min.js" : "dist/everyday-ew-image-card-webcomponent.js",
-			format: "iife",
-			esModule: false
-		},
-		plugins: [
-			resolve(),
-			commonjs(),
-			typescript(),
-			replace({
-				exclude: "node_modules/**", ENV: JSON.stringify(process.env.NODE_ENV || development),
-			}),
-			(process.env.NODE_ENV === production && uglify())
-		]
+		output: { ...output, file: process.env.NODE_ENV === production ? "dist/everyday-ew-image-card-webcomponent.min.js" : "dist/everyday-ew-image-card-webcomponent.js" },
+		plugins: plugins
 	},
 
 	// CommonJS (for Node) and ES module (for bundlers) build
