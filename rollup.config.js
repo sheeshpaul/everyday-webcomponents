60f9a3346ba57b1b429376c1bfec96985f6d1505
Sheeshpaul Kamboj <shekam@microsoft.com>
2019-10-02 23:52:49 -0700

    Updated rollup config
    Updated rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -5,13 +5,16 @@ import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript";
 import uglify from "rollup-plugin-uglify-es";
 
+const production = "production";
+const development = "development";
+
 export default [
-	// Browser-friendly iife build
+	// All components bundle
 	{
 		input: "src/main.ts",
 		output: {
 			name: "ew",
-			file: process.env.NODE_ENV !== "production" ? "dist/everyday-webcomponents.js" : pkg.browser,
+			file: process.env.NODE_ENV === production ? pkg.browser : "dist/everyday-webcomponents.js",
 			format: "iife",
 			esModule: false
 		},
@@ -20,9 +23,49 @@ export default [
 			commonjs(),   // Convert CommonJS modules to ES6, so they can be included in a Rollup bundle
 			typescript(), // Convert TypeScript to JavaScript
 			replace({
-				exclude: "node_modules/**", ENV: JSON.stringify(process.env.NODE_ENV || "development"),
+				exclude: "node_modules/**", ENV: JSON.stringify(process.env.NODE_ENV || development),
 			}),
-			(process.env.NODE_ENV === "production" && uglify())
+			(process.env.NODE_ENV === production && uglify())
+		]
+	},
+
+	// ew-card component bundle
+	{
+		input: "src/components/ew-card/index.ts",
+		output: {
+			name: "ew",
+			file: process.env.NODE_ENV === production ? "dist/everyday-ew-card-webcomponent.min.js" : "dist/everyday-ew-card-webcomponent.js",
+			format: "iife",
+			esModule: false
+		},
+		plugins: [
+			resolve(),
+			commonjs(),
+			typescript(),
+			replace({
+				exclude: "node_modules/**", ENV: JSON.stringify(process.env.NODE_ENV || development),
+			}),
+			(process.env.NODE_ENV === production && uglify())
+		]
+	},
+
+	// ew-image-card component bundle
+	{
+		input: "src/components/ew-image-card/index.ts",
+		output: {
+			name: "ew",
+			file: process.env.NODE_ENV === production ? "dist/everyday-ew-image-card-webcomponent.min.js" : "dist/everyday-ew-image-card-webcomponent.js",
+			format: "iife",
+			esModule: false
+		},
+		plugins: [
+			resolve(),
+			commonjs(),
+			typescript(),
+			replace({
+				exclude: "node_modules/**", ENV: JSON.stringify(process.env.NODE_ENV || development),
+			}),
+			(process.env.NODE_ENV === production && uglify())
 		]
 	},
 
